88493ab4363301f40918adc33cae6c8a5944960c
Vladimir Chernjakchovich <1svovik@gmail.com>
2022-09-18 16:23:13 +0300

    second println

--- src/Work15/Main.java
+++ src/Work15/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+
+        // New println
+        System.out.println("Second change");
     }
 }
